7d8982bc1877655965468f2b53df5b4969b955c7
DevBuike <142910858+DevBuike@users.noreply.github.com>
2025-07-09 22:55:50 +0000

    scaffolding the project

---
+++ direct-rental/src/components/layout/Header.jsx
@@ -0,0 +1,21 @@
+const Header = () => {
+  return (
+      <header className="bg-white fixed">
+        <div>
+          <div>
+            DirectRental
+          </div>
+          <div>
+            <ul>
+              <li>Home</li>
+              <li>About</li>
+              <li>FAQ</li>
+              <li>Contact Us</li>
+            </ul>
+          </div>
+        </div>
+      </header>
+  );
+}
+
+export default Header;
