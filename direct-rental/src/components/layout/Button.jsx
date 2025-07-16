97f66fea73f0911bc2310537442d6aac50cafab2
DevBuike <142910858+DevBuike@users.noreply.github.com>
2025-07-16 16:34:43 -0700

    Delete direct-rental directory

--- direct-rental/src/components/layout/Button.jsx
+++
@@ -1,9 +0,0 @@
-const Button = ({ label, className='', type='' }) => {
-  return (
-    <button className={`px-8 py-3 rounded ${className}`} >
-      { label }
-    </button>
-  )
-}
-
-export default Button;
